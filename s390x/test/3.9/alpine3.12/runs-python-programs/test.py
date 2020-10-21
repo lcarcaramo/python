a04bd2a1ed63b611fa9630c1788efaa601b8805c
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-10-21 12:26:16 -0400

    Create test.py

---
+++ s390x/test/3.9/alpine3.12/runs-python-programs/test.py
@@ -0,0 +1,4 @@
+from colors import *
+
+print(color("Success! This Python program is running on the quay.io/ibmz/python image!", fg='green'))
+print(color("Test Complete. Terminating program...", fg='green'))
